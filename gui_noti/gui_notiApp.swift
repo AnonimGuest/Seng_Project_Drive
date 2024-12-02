05eabeb8155a8c4afd66b56b1a7c6e88fb74a4eb
Ceren Askin <ceren@sektorsoft.com>
2024-12-01 23:41:49 -0500

    Initial Commit

---
+++ gui_noti/gui_notiApp.swift
@@ -0,0 +1,17 @@
+//
+//  gui_notiApp.swift
+//  gui_noti
+//
+//  Created by 𝐶. on 2024-12-01.
+//
+
+import SwiftUI
+
+@main
+struct gui_notiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gui_notiTests/gui_notiTests.swift
@@ -0,0 +1,17 @@
+//
+//  gui_notiTests.swift
+//  gui_notiTests
+//
+//  Created by 𝐶. on 2024-12-01.
+//
+
+import Testing
+@testable import gui_noti
+
+struct gui_notiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
